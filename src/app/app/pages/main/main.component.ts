36bc3813c380335eb894a103ab9c8c14d116192e
DM-Malkoff <demoncfh@mail.ru>
2024-09-25 23:40:55 +0400

    добавил таймер

--- src/app/app/pages/main/main.component.ts
+++ src/app/app/pages/main/main.component.ts
@@ -1,15 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DrawerComponent } from '../../components/drawer/drawer.component';
 import { RouterModule } from '@angular/router';
+import { TimeFormatPipe } from '../../pipes/time-format-pipe.pipe';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [DrawerComponent, RouterModule],
+  imports: [DrawerComponent, RouterModule, TimeFormatPipe],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
-export class MainComponent {
+export class MainComponent implements OnInit, OnDestroy{
+  public timeInSeconds = 10;
+  public initialTimerWidth = 10;
+  public currentTimerWidth = 0;
+  public maxTimerWidth = 200;
+  public timeIsOut = false;
+  private animationFrameId: number | null = null;
+  private intervalId: any;
+
+  constructor() {
+    this.currentTimerWidth = this.initialTimerWidth;
+  }
+
+  public ngOnInit() {
+    const totalDuration = this.timeInSeconds * 1000;
+    const startTime = performance.now();
+
+    const animate = (currentTime: number) => {
+      const elapsedTime = currentTime - startTime;
+
+      const remainingTime = totalDuration - elapsedTime;
+
+      if (remainingTime > 0) {
+        this.currentTimerWidth = this.maxTimerWidth * (1 - (remainingTime / totalDuration));
+        this.animationFrameId = requestAnimationFrame(animate);
+      } else {
+
+        this.currentTimerWidth = this.maxTimerWidth;
+        this.animationFrameId = null;
+      }
+    };
+
+    this.animationFrameId = requestAnimationFrame(animate);
+
+    this.intervalId = setInterval(() => {
+      if (this.timeInSeconds > 0) {
+        this.timeInSeconds--;
+      } else {
+        this.timeIsOut = true;
+        clearInterval(this.intervalId);
+      }
+    }, 1000);
+  }
+
+  public ngOnDestroy() {
+    if (this.animationFrameId) {
+      cancelAnimationFrame(this.animationFrameId);
+    }
+  }
+
   onClickViral(event: any) {
     const target = event.target;
     const imgElement =
@@ -19,7 +69,7 @@ export class MainComponent {
       imgElement.classList.add('shake');
       setTimeout(() => {
         imgElement.classList.remove('shake');
-      }, 2000); // 2 seconds
+      }, 2000);
     }
   }
 }
---
+++ src/app/app/pipes/time-format-pipe.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeFormat',
+  standalone: true,
+})
+export class TimeFormatPipe implements PipeTransform {
+  transform(totalSeconds: number): string {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+    return `${hours}h ${minutes}m ${seconds}s`;
+  }
+}
